1b497941f1a058cd02da89ec93dd4176b9a731e4
felipeverse <felipealvesrrodrigues@outlook.com>
2024-02-14 10:20:03 -0400

    - remove arquivos desnecessários - aplica novos estilos css - adiciona efeito partículas ao background - adiciona página de trabalho em andamento

---
+++ resources/js/particles-background.js
@@ -0,0 +1,7 @@
+var particles = Particles.init({
+    selector: '.background',
+    maxParticles:10,
+    sizeVariations:7,
+    color: ['#DA0463', '#404B69', '#DBEDF3', '#00FF00'],
+    connectParticles: true,
+});
